016c750982120c86a15ef568998a3d98e9b29a4e
TrishanBasu <trishanbasu2006@gmail.com>
2026-09-07 13:36:40 +0530

    Update fpl_api.py

--- fpl_api.py
+++ fpl_api.py
@@ -3,30 +3,72 @@ import requests
 
 def get_fpl_data():
     url = "https://fantasy.premierleague.com/api/bootstrap-static/"
-    
+
     response = requests.get(url)
     response.raise_for_status()
-    
+
     return response.json()
 
 
 def get_fixtures():
     url = "https://fantasy.premierleague.com/api/fixtures/"
-    
+
     response = requests.get(url)
     response.raise_for_status()
-    
+
     return response.json()
 
 
 def get_next_5_gameweeks(fixtures):
-    current_gameweeks = []
+
+    gameweeks = []
 
     for fixture in fixtures:
-        if not fixture["finished"] and fixture["event"] is not None:
-            if fixture["event"] not in current_gameweeks:
-                current_gameweeks.append(fixture["event"])
 
-    current_gameweeks.sort()
+        if (
+            not fixture["finished"]
+            and fixture["event"] is not None
+        ):
 
-    return current_gameweeks[:5]
+            if fixture["event"] not in gameweeks:
+                gameweeks.append(fixture["event"])
+
+    gameweeks.sort()
+
+    return gameweeks[:5]
+
+
+def calculate_fixture_difficulty(
+    fixtures,
+    team_id,
+    next_5_gws
+):
+
+    difficulties = []
+
+    for fixture in fixtures:
+
+        if (
+            fixture["event"] in next_5_gws
+            and not fixture["finished"]
+        ):
+
+            if fixture["team_h"] == team_id:
+
+                difficulties.append(
+                    fixture["team_h_difficulty"]
+                )
+
+            elif fixture["team_a"] == team_id:
+
+                difficulties.append(
+                    fixture["team_a_difficulty"]
+                )
+
+    if not difficulties:
+        return None
+
+    return round(
+        sum(difficulties) / len(difficulties),
+        2
+    )
